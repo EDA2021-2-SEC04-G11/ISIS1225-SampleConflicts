771a2fe4dff6ac75e07c1bed3b2e3674fb15a68a
Ronald Yesid Diaz Pardo <r.diazp@uniandes.edu.co>
2021-08-22 14:22:14 -0500

    Cambios finales

--- App/view.py
+++ App/view.py
@@ -36,7 +36,7 @@ Presenta el menu de opciones  y  por cada seleccion
 se hace la solicitud al controlador para ejecutar la
 operación solicitada
 """
-
+#test
 
 def printMenu():
     print("Opciones:")
